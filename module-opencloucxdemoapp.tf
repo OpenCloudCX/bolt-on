31db7ccb1bab4d1453dda5945c8cb0c8ffe3a8b2
Adam Norris <anorris@rivasolutionsinc.com>
2021-12-10 21:00:00 -0500

    Changed branch reference to demo

--- module-opencloucxdemoapp.tf
+++ module-opencloucxdemoapp.tf
@@ -1,6 +1,6 @@
 module "opencloudcxdemoapp" {
   # source = "../module-python-microservice"
-  source = "git::ssh://git@github.com/OpenCloudCX/module-bootstrapdemoapp?ref=develop"
+  source = "git::ssh://git@github.com/OpenCloudCX/module-bootstrapdemoapp?ref=demo"
 
   github_hook_pw = var.github_hook_pw
 
