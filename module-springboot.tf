78a62b24bfbf16578ef5d1bc033acdbf75aa17bd
Adam Norris <anorris@rivasolutionsinc.com>
2021-12-15 09:48:05 -0500

    Changed source for springboot module

--- module-springboot.tf
+++ module-springboot.tf
@@ -1,6 +1,6 @@
 module "springboot-microservice" {
-  source = "../module-springboot-microservice"
-  # source = "git::ssh://git@github.com/OpenCloudCX/module-springboot-microservice?ref=develop"
+  # source = "../module-springboot-microservice"
+  source = "git::ssh://git@github.com/OpenCloudCX/module-springboot-microservice?ref=develop"
 
   github_hook_pw = var.github_hook_pw
 
