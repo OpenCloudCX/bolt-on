2026c06b60a186eaf377b7aa38b177f654c77851
Adam Norris <anorris@rivasolutionsinc.com>
2022-02-28 23:23:30 -0500

    Removed unused parameters

--- module-sonarqube-config.tf
+++ module-sonarqube-config.tf
@@ -2,9 +2,6 @@ module "sonarqube-config" {
   # source = "../module-react-microservice"
   source = "git::ssh://git@github.com/OpenCloudCX/module-sonarqube-jenkins-config?ref=demo"
 
-  github_hook_pw    = var.github_hook_pw
-  sonarqube_api_key = var.sonarqube_api_key
-
   providers = {
     jenkins   = jenkins,
     sonarqube = sonarqube
