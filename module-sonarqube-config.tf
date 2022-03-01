8a91b64f3a67fdd978b89df61f558f2731d93dba
Adam Norris <anorris@rivasolutionsinc.com>
2022-02-28 22:34:17 -0500

    Fomratting and provider passthrough

--- module-sonarqube-config.tf
+++ module-sonarqube-config.tf
@@ -2,11 +2,12 @@ module "sonarqube-config" {
   # source = "../module-react-microservice"
   source = "git::ssh://git@github.com/OpenCloudCX/module-sonarqube-jenkins-config?ref=demo"
 
-  github_hook_pw = var.github_hook_pw
+  github_hook_pw    = var.github_hook_pw
   sonarqube_api_key = var.sonarqube_api_key
 
   providers = {
-    jenkins = jenkins
+    jenkins   = jenkins,
+    sonarqube = sonarqube
   }
 }
 
